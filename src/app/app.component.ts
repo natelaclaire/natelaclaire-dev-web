94482b937712f96a94ba42246504bd1a7aba70ec
Nate LaClaire <nate@natelaclaire.com>
2019-03-03 00:18:51 -0500

    Added LinkedIn icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faGithub, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,5 @@ export class AppComponent {
   faGithub = faGithub;
   faTwitter = faTwitter;
   faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
 }
